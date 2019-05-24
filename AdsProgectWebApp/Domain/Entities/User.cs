8575318a04563780cc339d32614b25f77e98f30f
remziasanov <35073857+remziasanov@users.noreply.github.com>
2019-05-24 22:39:15 +0300

    Added user

---
+++ AdsProgectWebApp/Domain/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+namespace Domain.Entities
+{
+    public class User : IdentityUser<Guid>
+    {
+        /// <summary>
+        /// ФИО пользователя
+        /// </summary>
+        public string FIO { get; set; }
+
+        /// <summary>
+        /// Регион пользователя
+        /// </summary>        
+        //public virtual Region UserRegion { get; set; }
+        public int RegionId { get; set; }
+
+        /// <summary>
+        /// Адрес пользователя
+        /// </summary>
+        public string UserAdress { get; set; }
+
+        /// <summary>
+        /// Статус пользователя. Забанен или нет?
+        /// </summary>
+        public string UserStatus { get; set; }
+    }
+}
